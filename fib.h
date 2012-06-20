a6c3a68e0b6c2c6e69324eb36b0460916cf8e12e
Nagase Tomohiko <tomohikoseven0905@gmail.com>
2012-06-20 21:07:23 +0900

    first commit

---
+++ fib.h
@@ -0,0 +1,15 @@
+#ifndef WORLD
+/**
+ * @brief hello world表示.
+ * @param[in] fib_n
+ */
+void print_hw( int fib_n );
+#endif  // WORLD
+
+#ifdef KITTY
+/**
+ * @brief hello kitty表示.
+ * @param[in] x_n
+ */
+void print_ky( int x_n );
+#endif  // KITTY
